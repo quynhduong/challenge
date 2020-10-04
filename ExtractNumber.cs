bbf5ec47165533a1886ad21ca956361fb23ffee5
m6122040121 <m6122040121@g.siit.tu.ac.th>
2020-10-04 14:31:09 +0700

    solution added

---
+++ ExtractNumber.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace test
+{
+    public class ExtractNumber
+    {
+        public int extractnumber(string original)
+        {
+            string output = "";
+            string new_output = "";
+
+            for (int i = 0; i < original.Length; i++)
+            {
+                if (original[i] == '0' || original[i] == '1' || original[i] == '2' || original[i] == '3' ||
+                    original[i] == '4' || original[i] == '5' || original[i] == '6' || original[i] == '7' ||
+                    original[i] == '8' || original[i] == '9')
+                {
+                    output += original[i];
+                    Console.WriteLine(output);
+                }
+            }
+
+
+            for (int j = 0; j < output.Length; j++)
+            {
+                if (output[j] == '0' || output[j] == '1')
+                {
+                    new_output += CharToStr(output[j]);
+                }
+                else
+                {
+                    new_output += output[j];
+                }
+            }
+
+           
+
+            return StrToInt(new_output); ;
+
+
+        }
+        public int StrToInt(string str)
+        {
+            int response = 0;
+
+            foreach (char c in str)
+            {
+                response *= 10;
+                response += c - '0';
+            }
+            return response;
+        }
+        public string CharToStr(char Char)
+        {
+            string cString = new string(new char[] { Char });
+
+            return cString;
+        }
+       
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace test
+{
+    public class Program
+    {
+        public static ExtractNumber extract = new ExtractNumber();
+        static void Main(string[] args)
+        {
+            while(!Console.KeyAvailable)
+            {
+                Console.WriteLine("Please input your string: ");
+
+                //Input a string
+                string input = Console.ReadLine();
+             
+                // Output is an interger
+                int output = Convert.ToInt32(extract.extractnumber(input));
+
+                Console.WriteLine("Output  = " + output);
+
+
+            }
+
+        }
+
+    }
+
+    
+}
